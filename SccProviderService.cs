9406b0de41312808b59a705d3f55f893866d2826
Yiyi Sun <yiyisun@gmail.com>
2011-10-09 09:40:56 -0400

    history windows as a standalone program

--- SccProviderService.cs
+++ SccProviderService.cs
@@ -1118,9 +1118,9 @@ _ReSharper*/
                 as PendingChangesToolWindow;
             if (window != null) window.Refresh(this.CurrentTracker);
 
-            var window2 = this._sccProvider.FindToolWindow(typeof(HistoryToolWindow), 0, false)
-                as HistoryToolWindow;
-            if (window2 != null) window2.Refresh(this.CurrentTracker);
+            //var window2 = this._sccProvider.FindToolWindow(typeof(HistoryToolWindow), 0, false)
+            //    as HistoryToolWindow;
+            //if (window2 != null) window2.Refresh(this.CurrentTracker);
         }
     }
 }
